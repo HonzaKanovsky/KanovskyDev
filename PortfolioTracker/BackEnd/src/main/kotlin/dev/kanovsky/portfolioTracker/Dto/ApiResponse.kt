8104f5c062393bf4ba4f29353599e7c665eaf57f
Jan Kanovsky <honza@kanovsky.net>
2025-01-03 17:09:16 +0100

    Modify ApiResponse class

--- PortfolioTracker/BackEnd/src/main/kotlin/dev/kanovsky/portfolioTracker/Dto/ApiResponse.kt
+++ PortfolioTracker/BackEnd/src/main/kotlin/dev/kanovsky/portfolioTracker/Dto/ApiResponse.kt
@@ -1,7 +1,12 @@
 package dev.kanovsky.portfolioTracker.Dto
 
+import java.sql.Timestamp
+import java.time.LocalDateTime
+import java.util.Date
+
 data class ApiResponse<T> (
     val success: Boolean,
     val message: String,
+    val timestamp: LocalDateTime? = LocalDateTime.now(),
     val data: T? = null
 )
