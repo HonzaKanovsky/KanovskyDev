f1c359cac12aa6a2938d54309b287c8b887fadb9
HonzaKanovsky <honza@kanovsky.net>
2025-01-30 10:07:36 +0900

    Update password regex pattern

--- PortfolioTracker/BackEnd/src/main/kotlin/dev/kanovsky/portfolioTracker/enums/RegexPatterns.kt
+++ PortfolioTracker/BackEnd/src/main/kotlin/dev/kanovsky/portfolioTracker/enums/RegexPatterns.kt
@@ -5,7 +5,7 @@ import java.util.regex.Pattern
 enum class RegexPatterns(val pattern: String) {
     USERNAME("^[A-Za-z\\d]+$"),
     EMAIL("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"),
-    PASSWORD("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&.,])[A-Za-z\\d@$!%*?&.,]{6,}$");
+    PASSWORD("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_])[A-Za-z\\d\\W_]{6,}$");
 
     fun matches(input: String): Boolean {
         return Pattern.compile(pattern).matcher(input).matches()
