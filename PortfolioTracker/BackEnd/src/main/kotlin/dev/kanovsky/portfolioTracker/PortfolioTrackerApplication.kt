e352a37d1d3a44691b11247511d2d47c7146add5
HonzaKanovsky <honza@kanovsky.net>
2025-01-30 15:54:46 +0900

    EnableScheduling

--- PortfolioTracker/BackEnd/src/main/kotlin/dev/kanovsky/portfolioTracker/PortfolioTrackerApplication.kt
+++ PortfolioTracker/BackEnd/src/main/kotlin/dev/kanovsky/portfolioTracker/PortfolioTrackerApplication.kt
@@ -2,10 +2,12 @@ package dev.kanovsky.portfolioTracker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class PortfolioTrackerApplication
 
 fun main(args: Array<String>) {
-	runApplication<PortfolioTrackerApplication>(*args)
+    runApplication<PortfolioTrackerApplication>(*args)
 }
